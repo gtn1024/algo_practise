19d3982dd95b8d215f7b7b2a66d58053ec7d6ffd
Taoning Ge <gtn1024o@outlook.com>
2021-12-11 22:59:14 +0800

    2021.12.11洛谷打卡

---
+++ src/luogu/rm1/p1425/Main.java
@@ -0,0 +1,16 @@
+package luogu.rm1.p1425;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int allMin = (c - a - 1) * 60 + (60 - b) + d;
+        System.out.println(allMin / 60 + " " + allMin % 60);
+    }
+}
---
+++ src/luogu/rm2/p5711/Main.java
@@ -0,0 +1,14 @@
+package luogu.rm2.p5711;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.println(
+                (i % 4 == 0 && i % 100 != 0) || (i % 400 == 0) ?
+                        1 : 0
+        );
+    }
+}
